02b61036a4d3baaf90add150b5ae203fc9e6c323
jackh2 <jackh2@vt.edu>
2023-05-02 23:38:33 -0400

    cleaned code

--- scraper.py
+++ scraper.py
@@ -9,45 +9,38 @@ import pickle
 logging.basicConfig(level=logging.INFO, filename="scraper.log")
 
 def main():
-	urls = ["/author/anton-chekhov/short-story/uprooted"]
-	# for i in range(1):
-	# 	url = "https://americanliterature.com/short-story-library/?page=" + str(19)
-	# 	page = requests.get(url)
-	# 	soup = BeautifulSoup(page.content, "html.parser")
-	# 	link_list = soup.find("div", class_="row")
-	# 	urls.extend([link.get('href') for link in link_list.findAll('a')])
-	# logging.debug(f"Urls received: [{', '.join(urls)}] ")
+	urls = []
+	for i in range(1,20):
+		url = "https://americanliterature.com/short-story-library/?page=" + str(i)
+		page = requests.get(url)
+		soup = BeautifulSoup(page.content, "html.parser")
+		link_list = soup.find("div", class_="row")
+		urls.extend([link.get('href') for link in link_list.findAll('a')])
+	logging.debug(f"Urls received: [{', '.join(urls)}] ")
 
 
 	db, collection = connect_to_mongo()
 	for url in urls:
-		# try:
-		s = url.split("/")
-		title = s[4]
-		author = s[2]
-		x = get_short_story(url, title, author)
-		logging.info(f"story object: {x}")
-		# if collection.count_documents({"title": title}) == 0:
-		# 	collection.insert_one(get_short_story(url, title, author))
-		# else:
-		# 	print("skipped:", title)
-		# except Exception as e:
-		# 	print(e)
-		# 	logging.error(f"Error: {e} for URL {url}")
+		try:
+			s = url.split("/")
+			title = s[4]
+			author = s[2]
+			if collection.count_documents({"title": title}) == 0:
+				short_story = get_short_story(url, title, author)
+				logging.debug(f"story object: {short_story}")
+				collection.insert_one(short_story)
+			else:
+				print("skipped:", title)
+		except Exception as e:
+			logging.error(f"Error: {e} for URL {url}")
 
 
 def get_short_story(url, title, author):
 	full_url = "https://americanliterature.com" + url
-	# page = requests.get(full_url)
-	# with open("test_story.pickle","wb") as f:
-	# 	pickle.dump(page.content, f)
-	# soup = BeautifulSoup(page.content, "html.parser")
-	content = pickle.load(open("test_story.pickle", "rb"))
-	soup = BeautifulSoup(content, "html.parser")
+	page = requests.get(full_url)
+	soup = BeautifulSoup(page.content, "html.parser")
 	story_html = soup.find('div', {"itemtype":"https://schema.org/ShortStory"})
-	print(story_html)
 	unparsed_story = story_html.findAll('p')
-	parsed = []
 	line = unparsed_story[0]
 	html_cleaned_story = cleanhtml(str(line))
 	parsed = list(filter(None, html_cleaned_story.split("\n")))
